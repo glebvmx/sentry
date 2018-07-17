7298ee63b9da934f91841849cfc779f1d362277e
glebvmx <40228716+glebvmx@users.noreply.github.com>
2018-07-17 14:11:52 +0300

    Update sentry.sh

--- sentry.sh
+++ sentry.sh
@@ -6,8 +6,8 @@ cd /opt/docker/sentry
 mkdir -p data/{sentry,postgres}
 docker-compose run --rm web config generate-secret-key
 SENTRY_SECRET_KEY=`docker-compose run --rm web config generate-secret-key`
+sed "s/#SENTRY_SECRET_KEY:/SENTRY_SECRET_KEY: $SENTRY_SECRET_KEY/g" docker-compose.yml > docker-compose-s.yml
 docker-compose run  -e SENTRY_SECRET_KEY=$SENTRY_SECRET_KEY --rm web upgrade --noinput
 docker-compose run  -e SENTRY_SECRET_KEY=$SENTRY_SECRET_KEY --rm web createuser --email glebvmx@gmail.com --password secret --superuser
-sed "s/# SENTRY_SECRET_KEY:/SENTRY_SECRET_KEY: $SENTRY_SECRET_KEY/g" docker-compose.yml > docker-compose-s.yml
 /usr/local/bin/docker-compose -f docker-compose-s.yml up -d
 
